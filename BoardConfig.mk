cd55c32b5d762e08a31634f3d675d0aeb057c000
SamarV-121 <samarvispute121@gmail.com>
2019-06-10 11:12:11 +0530

    RMX1851: Enable AVB
    Change-Id: I076ef450b7762ed21d76a5e18f70b2d0dab0fec2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Avb
+BOARD_AVB_ENABLE := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm710
 TARGET_NO_BOOTLOADER := true
